ea1a5a16efea355bc0bde833a8a8e9506bc804c2
Erickson Mbuvi <mutisyaerickson@gmail.com>
2024-10-05 16:37:37 -0400

    added files andd fixed issue with model

---
+++ api/index.py
@@ -0,0 +1,72 @@
+"""Script to create a Flask API for the model."""
+import sys
+import joblib
+from flask import Flask, request, jsonify
+import keras
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from nltk import pos_tag, word_tokenize
+import nltk
+
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+app = Flask(__name__)
+
+
+# function to add POS TAGS to text
+def add_pos(text):
+    """Adds POS tags to the text
+    Args: 
+        text: str
+    Returns:
+        str: text with POS tags
+    """
+    # tokenization
+    tokens = word_tokenize(text)
+    # obtain tags
+    pos_tags = pos_tag(tokens)
+    tags = [x[1] for x in pos_tags]
+    
+    # add tags
+    pos_added_tokens = ['_'.join(item) for item in list(zip(tokens, tags))]
+    
+    # join to single string
+    pos_text = ' '.join(pos_added_tokens)
+    
+    return pos_text
+
+
+@app.route('/api/detect', methods=['POST'])
+def detect():
+    """Predicts if text is ai generated or not."""
+    # load model
+    model = tf.keras.models.load_model('api/model.h5')    
+    
+    # vectorizer
+    with open('api/vectorizer.pkl', 'rb') as f:
+        vectorizer = joblib.load(f)
+        
+    # get data
+    data = request.get_json(force=True)
+    text = data['text'].encode('utf-8').decode('utf-8')
+    
+    # add pos tags
+    text = add_pos(text)
+    
+    # vectorize
+    text_vectorized = vectorizer.transform([text])
+    
+    # predict
+    prediction = model.predict(text_vectorized[0])[0]
+    print(prediction)
+    
+    result = 0 if prediction < 0.5 else 1
+    
+    return jsonify({'prediction': int(result)})
---
+++ app/ui/text-form.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { useState } from "react"
+
+
+export default function TextForm(
+    {setResults: setResults}: {setResults: (results: number) => void}) {
+    const [text , setText] = useState('')
+    const [detecting , setDetecting] = useState(false)
+
+    function handleSubmit(e: React.MouseEvent<HTMLButtonElement>) {
+        e.preventDefault()
+        setResults(-1)
+        setDetecting(true)
+        fetch('/api/detect', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({text})
+        })
+        .then(res => res.json())
+        .then(data => {
+            setResults(data.prediction)
+            console.log(data)
+            setDetecting(false)
+        })
+        .catch(err => {
+            alert('An error occurred. Please try again.')
+            console.log(err)
+            setDetecting(false)
+        })
+    }
+
+    return (
+        <form className="flex flex-col p-4 md:w-1/2" >
+            <label className="text-lg">Enter text to detect:</label>
+            <textarea
+                className="border p-2 rounded mt-2 text-black"
+                placeholder="Enter text here"
+                rows={5}
+                cols={50}
+                onChange={(e) => setText(e.target.value)}
+                value={text}
+            />
+            <div className="flex gap-2 items-center w-full justify-between">
+                {detecting?
+                    // spinner
+                    <div className="border border-t-4 border-gray-200 rounded-full w-6 h-6 animate-spin"></div>
+                    : 
+                <button 
+                    className="bg-blue-500 text-white rounded p-2 mt-2"
+                    onClick={handleSubmit}>Detect</button>
+                }
+                {/* clear button */}
+                <div 
+                    className="bg-red-500 text-white rounded p-2 mt-2 cursor-pointer"
+                    onClick={() => setText('')}>Clear</div>
+            </div>
+        </form>
+    )
+}
